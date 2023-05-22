5dd992da0f5fb5e93422473c639edeeaa431874b
Aemal <aemal7@icloud.com>
2023-05-22 12:11:47 +0500

    add to cart button

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { StoreProvider } from "./Store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
